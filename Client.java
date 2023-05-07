389deebad5814bad47c099f09e7e519f24a0cf4a
Komal Pathare <117629456+komalpathare8755@users.noreply.github.com>
2023-05-07 15:56:18 +0530

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,15 +1,57 @@
-imoprt java.net.*;
-imoprt java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
 
-class Client
+class MarvellousFrame
 {
-	public static void main(String A[])
+    public CHATTINGAPPFrame(String title)
+    {
+        Frame fobj = new Frame(title);
+        fobj.setSize(500,500);
+        fobj.setVisible(true);
+
+        Button bobj =new Button("SEND");
+		fobj.add(bobj);
+        fobj.addWindowListener(new MarvellousListener());
+    }
+}
+
+class CHATTINGAPP_APP extends WindowAdapter
+{
+    
+    public void windowClosing(WindowEvent obj)
 	{
-		System.out.println("Client application is runnning ...");
-		
-		Socket s=new Socket("localhost",2100);
-		System.out.println("Client is waiting for the server to accept the request");
-		
+		System.exit(0);
 	}
-	
+}
+
+
+
+public class Client
+{
+    public static void main(String A[]) throws Exception
+    {
+        CHATTINGAPPFrame cobj =new CHATTINGAPPFrame(""😘CHATTING_APP😘);
+        
+        System.out.println("Client application is running...");
+
+        Socket s = new Socket("localhost",2100);
+        System.out.println("Client is waiting for the server to accept the request");
+
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1, str2;
+
+        while(!(str1 = br2.readLine()).equals("end"))
+        {
+            ps.println(str1);
+            System.out.print("Enter message for server : ");
+            str2 = br1.readLine();
+            System.out.println("Server says : "+str2);
+        }
+    }
 }
--- Server.java
+++ Server.java
@@ -1,10 +1,61 @@
-imoprt java.net.*;
-imoprt java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
 
-class Server{
-	public static void main(String A[])
+
+class CHATTING_APPFrame
+{
+    public CHATTING_APPFrame(String title)
+    {
+        Frame fobj = new Frame(title);
+        fobj.setSize(500,500);
+        fobj.setVisible(true);
+
+        Button bobj =new Button("Send");
+		fobj.add(bobj);
+        fobj.addWindowListener(new CHATTING_APP());
+    }
+}
+
+class CHATTING_APP extends WindowAdapter
+{
+    
+    public void windowClosing(WindowEvent obj)
 	{
-		System.out.println("Server application is runnning ...");
+		System.exit(0);
 	}
-	
+}
+
+
+
+public class Server
+{
+    public static void main(String A[]) throws Exception
+    {
+        CHATTING_APPFrame cobj=new CHATTING_APPFrame("😘CHATTING_APP😘");
+		System.out.println("Server application is running...");
+
+        ServerSocket ss = new ServerSocket(2100);
+        System.out.println("Server is running at port no 2100 and waiting for client request");
+
+        Socket s = ss.accept();
+        System.out.println("Request of client gets accepted");
+
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str1;
+        String	str2;
+
+        while((str1 = br1.readLine()) != null)
+        {
+            ps.println(str2);
+            System.out.println("Client says : "+str1);
+            str2 = br2.readLine();
+            System.out.print("Enter message for client : ");
+        }
+    }
 }
